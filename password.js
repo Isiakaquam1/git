171ae797e749ea266d0292f7ded765e40d4ce628
Isiakaquam1 <159121506+Isiakaquam1@users.noreply.github.com>
2024-03-13 08:43:57 -0700

    update

---
+++ password.js
@@ -0,0 +1,30 @@
+const input1 = document.getElementById('password')
+
+const input2 = document.getElementById('comfirmPassword')
+
+function pass() {
+ if (password.value == confirmPassword.value) {
+     alert('password match')
+
+ } else {
+     alert('password mismatch')
+ }
+ 
+
+
+}
+
+let input = document.getElementById('eligibility')
+
+function age() {
+ 
+ if (eligibility.value == 18) {
+     alert('you are eligible')
+ } else {
+     alert('you are not eligible')
+     
+ }
+
+
+
+}
