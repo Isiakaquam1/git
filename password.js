c9819067d2b6e74a8920863344ecb11976142f28
Isiakaquam1 <159121506+Isiakaquam1@users.noreply.github.com>
2024-03-14 06:53:17 -0700

    array

--- password.js
+++ password.js
@@ -25,6 +25,32 @@ function age() {
      
  }
 
-
-
 }
+   let names = ["Victot", "Samuel", "David"];
+    console.log(names);
+     names.push("ola");
+    console.log(names);
+    names.shift('quam')
+    console.log(names);
+    names.unshift('ade');
+    console.log(names);
+    
+
+  input = document.getElementById("display");
+    function push(params) {
+        names.push(input.value);
+        console.log(names);
+     }
+
+     function pop(params) {
+        names.pop(input.value);
+        console.log(names);
+     }
+     function unshift(params) {
+        names.unshift(input.value);
+        console.log(names);
+     }
+     function shift(params) {
+        names.shift(input.value);
+        console.log(names);
+     }
